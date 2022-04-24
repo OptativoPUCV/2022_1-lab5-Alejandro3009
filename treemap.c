288e175f287f85b4061036555c7408e3399cd089
Your Name <you@example.com>
2022-04-24 14:49:13 -0300

    new try

--- treemap.c
+++ treemap.c
@@ -173,8 +173,6 @@ Pair * nextTreeMap(TreeMap * tree) {
         //TreeNode * actual = tree->current;
         while(1)
         {
-            if(aux == NULL)break;
-
             if(aux->right != NULL)
             {
                 if(aux->parent->right != aux)
